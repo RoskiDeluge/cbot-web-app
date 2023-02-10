248cc00954f940793df33535943aa92bc5e8ea8f
RoskiDeluge <roberto.delgado@gmail.com>
2023-02-10 11:27:11 -1000

    Edited prompt

--- pages/api/orchidbot.ts
+++ pages/api/orchidbot.ts
@@ -27,11 +27,11 @@ export default async function handler(
     prompt: `I am a prompt generator for Stable Diffusion and Dalle2.\n
     Give me the name of two artists and I will generate a prompt that combines their styles.\n
     Artists: Rene Magritte and Georgia O'Keffe\n
-    Prompt: A surreal image of an orchid floating in outer space in the style of Rene Magritte and Georgia O'Keffe\n
+    A surreal image of an orchid floating in outer space in the style of Rene Magritte and Georgia O'Keffe\n
     Artists: Frida Kahlo and Ansel Adams\n
-    Prompt: A hyperrealistic photo of a burning orchid in the style of Frida Kahlo and Ansel Adams\n
+    A hyperrealistic photo of a burning orchid in the style of Frida Kahlo and Ansel Adams\n
     Artists: Vincent Van Gough and Takashi Murakami\n
-    Prompt: An abstract image of an orchid explosion in a futuristic landscape in the style of Vincent Van gough and Takashi Murakami\n
+    An abstract image of an orchid explosion in a futuristic landscape in the style of Vincent Van gough and Takashi Murakami\n
     Artists: ${prompt}\n`,
     max_tokens: 200,
     temperature: 1, 
