07305e8b10e57e3a12d9ed959cede1a553e913bd
RoskiDeluge <roberto.delgado@gmail.com>
2023-03-05 00:09:46 -1000

    Changed orchidbot.ts to message format

--- pages/api/orchidbot.ts
+++ pages/api/orchidbot.ts
@@ -22,24 +22,38 @@ export default async function handler(
     return res.status(400).json({error: "Prompt too long"})
   }
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `I am a prompt generator for Stable Diffusion and Dalle2.\n
-    Give me the name of two artists and I will generate a prompt that combines their styles.\n
-    Artists: Rene Magritte and Georgia O'Keffe\n
-    A surreal image of an orchid floating in outer space in the style of Rene Magritte and Georgia O'Keffe\n
-    Artists: Frida Kahlo and Ansel Adams\n
-    A hyperrealistic photo of a burning orchid in the style of Frida Kahlo and Ansel Adams\n
-    Artists: Vincent Van Gough and Takashi Murakami\n
-    An abstract image of an orchid explosion in a futuristic landscape in the style of Vincent Van gough and Takashi Murakami\n
-    Artists: ${prompt}\n`,
-    max_tokens: 200,
-    temperature: 1, 
-    presence_penalty: 0,
-    frequency_penalty: 0,
+  // const completion = await openai.createCompletion({
+  //   model: "text-davinci-003",
+  //   prompt: `I am a prompt generator for Stable Diffusion and Dalle2.\n
+  //   Give me the name of two artists and I will generate a prompt that combines their styles.\n
+  //   Artists: Rene Magritte and Georgia O'Keffe\n
+  //   A surreal image of an orchid floating in outer space in the style of Rene Magritte and Georgia O'Keffe\n
+  //   Artists: Frida Kahlo and Ansel Adams\n
+  //   A hyperrealistic photo of a burning orchid in the style of Frida Kahlo and Ansel Adams\n
+  //   Artists: Vincent Van Gough and Takashi Murakami\n
+  //   An abstract image of an orchid explosion in a futuristic landscape in the style of Vincent Van gough and Takashi Murakami\n
+  //   Artists: ${prompt}\n`,
+  //   max_tokens: 200,
+  //   temperature: 1, 
+  //   presence_penalty: 0,
+  //   frequency_penalty: 0,
+  // })
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {"role": "system", "content": "You are a prompt generator for Stable Diffusion and Dalle2. You will generate a prompt that combines the styles of two artists."},
+      {"role": "user", "content": "Rene Magritte and Georgia O'Keffe"},
+      {"role": "assistant", "content": "A surreal image of an orchid floating in outer space in the style of Rene Magritte and Georgia O'Keffe"},
+      {"role": "user", "content": "Frida Kahlo and Ansel Adams"},
+      {"role": "assistant", "content": "A hyperrealistic photo of a burning orchid in the style of Frida Kahlo and Ansel Adams"},
+      {"role": "user", "content": "Vincent Van Gough and Takashi Murakami"},
+      {"role": "assistant", "content": "An abstract image of an orchid explosion in a futuristic landscape in the style of Vincent Van gough and Takashi Murakami"},
+      {"role": "user", "content": `"${prompt}"`},
+    ]
   })
 
-  const command =  completion.data.choices[0].text;
+  const command =  completion.data.choices[0].message?.content;
 
   res.status(200).json({command})
 }
